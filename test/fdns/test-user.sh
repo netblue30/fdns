945a24a0848269ad6b972809e7f363d907158e61
netblue30 <netblue30@yahoo.com>
2019-12-07 09:50:01 -0500

    testing

--- test/fdns/test-user.sh
+++ test/fdns/test-user.sh
@@ -6,10 +6,8 @@
 export MALLOC_CHECK_=3
 export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
 
-echo "TESTING: ********************"
-printf "TESTING: running as user "
-whoami
-echo "TESTING: ********************"
+echo "TESTING: stress (test/fdns/stress.exp)"
+./stress.exp
 
 echo "TESTING: list (test/fdns/list.exp)"
 ./list.exp
