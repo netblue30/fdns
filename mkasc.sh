c2c0b22efd3dae17256713e140e46d476e15be57
netblue30 <netblue30@yahoo.com>
2019-12-12 15:03:53 -0500

    postrelease

---
+++ mkasc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Calculationg SHA256 for all files in /transfer - fdns version $1"
+
+cd /transfer
+sha256sum * > fdns-$1-unsigned
+gpg --clearsign --digest-algo SHA256 < fdns-$1-unsigned > fdns-$1.asc
+gpg --verify fdns-$1.asc
+gpg --detach-sign --armor fdns-$1.tar.xz
+rm fdns-$1-unsigned
