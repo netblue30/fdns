285434d3f3777b46c346c8650b23f3cd0241bc67
netblue30 <netblue30@yahoo.com>
2020-05-26 13:26:02 -0400

    adding dns.sb

--- src/fdns/server.c
+++ src/fdns/server.c
@@ -142,11 +142,10 @@ static DnsServer *read_one_server(FILE *fp, int *linecnt, const char *fname) {
 			if (!s->address)
 				errExit("strdup");
 
-// todo: accept a server name in parallel with IP addresses
 			// check address:port
 			// commons.host is a geocast host
 			// OpenSSL will find out the IP address using regular DNS over UDP
-			if (strcmp(s->name, "commons.host") != 0) {
+			if (strcmp(s->name, "commons-host") != 0) {
 				if (check_addr_port(s->address)) {
 					fprintf(stderr, "Error: file %s, line %d, invalid address:port\n", fname, *linecnt);
 					exit(1);
