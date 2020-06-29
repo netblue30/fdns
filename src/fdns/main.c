4162f02161eda3d2d7a589cfb55c404ce70fbca9
netblue30 <netblue30@yahoo.com>
2020-06-29 12:27:39 -0400

    more randomization

--- src/fdns/main.c
+++ src/fdns/main.c
@@ -308,7 +308,7 @@ int main(int argc, char **argv) {
 	DnsServer *s = server_get();
 	assert(s);
 	assert(arg_server);
-	
+
 	// reinitialize random number generator (eache resolver seeded differently)
 	srand(time(NULL) + arg_id);
 
@@ -322,6 +322,10 @@ int main(int argc, char **argv) {
 		if (!arg_fallback_only) {
 			logprintf("connecting to %s server\n", s->name);
 			logprintf("\t%s\n", s->tags);
+			if (s->keepalive_min == s->keepalive_max)
+				logprintf("\tkeepalive %d seconds\n", s->keepalive_min);
+			else
+				logprintf("\tkeepalive %d - %d seconds\n", s->keepalive_min, s->keepalive_max);
 		}
 		frontend();
 	}
